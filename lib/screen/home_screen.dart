ec0f55c19bbf61ca1475cbe1ef6e5846c5b9c74b
Muslimin Ontong <moshOntong@gmail.com>
2023-01-16 00:38:51 +0800

    Update home_screen.dart
    added column widget

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,25 +5,44 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ///In Flutter, a scaffold is a basic skeletal structure that provides
-    ///support for visual elements such as the app bar, bottom navigation bar,
-    ///and floating action button. It also provides a framework to implement
-    ///drawer and bottom sheet behavior. The Scaffold widget is a Material
-    ///Design container that provides a visual structure for an app's layout.
+    ///* In Flutter, a scaffold is a basic skeletal structure that provides
+    ///* support for visual elements such as the app bar, bottom navigation bar,
+    ///* and floating action button. It also provides a framework to implement
+    ///* drawer and bottom sheet behavior. The Scaffold widget is a Material
+    ///* Design container that provides a visual structure for an app's layout.
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           'Hello',
         ),
       ),
-
-      ///notch, status bar, or navigation bar. as the
-      body: const SafeArea(
-        child: Center(
-          child: Text(
-            'Hello World',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              color: Colors.blue,
+              height: 100,
+              width: 100,
+            ),
           ),
-        ),
+
+          ///* The SizedBox widget that is used in the provided code snippet
+          ///* is used to create a fixed amount of empty space between the two
+          ///* Container widgets. The height property of the SizedBox widget is
+          ///* set to 50, which means that there will be 50 pixels of empty
+          ///* space between the two Container widgets.
+          const SizedBox(
+            height: 50,
+          ),
+          Center(
+            child: Container(
+              color: Colors.red,
+              height: 100,
+              width: 100,
+            ),
+          ),
+        ],
       ),
     );
   }
