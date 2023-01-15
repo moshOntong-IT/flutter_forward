e9955c6c1c6bc37ad60357070722a3dfd87bfc6c
Muslimin Ontong <moshOntong@gmail.com>
2023-01-16 01:04:57 +0800

    Update home_screen.dart

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,12 +2,23 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int counter = 0;
+  @override
   Widget build(BuildContext context) {
-    int counter = 0;
+    ///* When the setState method is called, the widget's build method is triggered,
+    ///*  which causes the widget to be rebuilt with the updated state.
+    ///* This process is known as rebuilding the widget tree.
+    ///* Therefore, the int counter variable must be not here inside the build method.
+    ///*  It must be outside the build method.
+    /// ! int counter = 0;
 
     ///* In Flutter, a scaffold is a basic skeletal structure that provides
     ///* support for visual elements such as the app bar, bottom navigation bar,
@@ -27,7 +38,13 @@ class HomeScreen extends StatelessWidget {
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            counter++;
+            ///*  when a button is pressed and the onPressed callback contains
+            ///* a call to setState, the state of the widget is updated and
+            ///* the widget's build method is triggered.
+            setState(() {
+              counter++;
+            });
+
             log('You have pushed the button this many times: $counter');
           },
           tooltip: 'Increment',
