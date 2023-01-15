a4dcc00575a8973388a1bfedf51b4dc771bb00e0
Muslimin Ontong <moshOntong@gmail.com>
2023-01-16 00:59:08 +0800

    Update home_screen.dart
    added floating action button and text

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,45 +7,31 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int counter = 0;
+
     ///* In Flutter, a scaffold is a basic skeletal structure that provides
     ///* support for visual elements such as the app bar, bottom navigation bar,
     ///* and floating action button. It also provides a framework to implement
     ///* drawer and bottom sheet behavior. The Scaffold widget is a Material
     ///* Design container that provides a visual structure for an app's layout.
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Hello',
+        appBar: AppBar(
+          title: const Text(
+            'Hello',
+          ),
         ),
-      ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Container(
-              color: Colors.blue,
-              height: 100,
-              width: 100,
-            ),
+        body: Center(
+          child: Text(
+            'You have pushed the button this many times: $counter',
           ),
-
-          ///* The SizedBox widget that is used in the provided code snippet
-          ///* is used to create a fixed amount of empty space between the two
-          ///* Container widgets. The height property of the SizedBox widget is
-          ///* set to 50, which means that there will be 50 pixels of empty
-          ///* space between the two Container widgets.
-          const SizedBox(
-            width: 50,
-          ),
-          Center(
-            child: Container(
-              color: Colors.red,
-              height: 100,
-              width: 100,
-            ),
-          ),
-        ],
-      ),
-    );
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            counter++;
+            log('You have pushed the button this many times: $counter');
+          },
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ));
   }
 }
